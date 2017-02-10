2e9144551099bf29f1c0e403a541bf8fc0c76008
Luis Cabrera <loudigi@gmail.com>
2017-02-09 19:35:47 -0500

    First commit message Hi

---
+++ MyFavBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFavBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("My fav book is \"The Alchemist\"");
+            Console.WriteLine("\a");
+            Console.WriteLine("\t Luis \n Cabrera");
+
+        
+           
+        }
+    }
+}
